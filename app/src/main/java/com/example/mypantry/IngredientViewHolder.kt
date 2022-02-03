b073a4c9af57773311905c373cfc0acce2dabc0f
selmatojcic <stojcic@etfos.hr>
2022-02-03 14:43:17 +0100

    displaying correct images

--- app/src/main/java/com/example/mypantry/IngredientViewHolder.kt
+++ app/src/main/java/com/example/mypantry/IngredientViewHolder.kt
@@ -1,7 +1,6 @@
 package com.example.mypantry
 
 import android.view.View
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.mypantry.databinding.ItemIngredientBinding
@@ -10,12 +9,12 @@ class IngredientViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(ingredient: Ingredient) {
         val itemIngredientBinding = ItemIngredientBinding.bind(itemView)
+        val imageUrl = "https://spoonacular.com/cdn/ingredients_100x100/" + ingredient.image
 
         itemIngredientBinding.tvIngredientName.text = ingredient.name
 
         Glide.with(itemView)
-//            .load(ingredient.image)
-            .load("https://spoonacular.com/cdn/ingredients_100x100/apple.jpg")
+            .load(imageUrl)
             .into(itemIngredientBinding.ivIngredient)
 
     }
--- app/src/main/java/com/example/mypantry/IngredientViewModel.kt
+++ app/src/main/java/com/example/mypantry/IngredientViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.mypantry
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
